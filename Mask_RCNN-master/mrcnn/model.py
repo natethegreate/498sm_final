f0646af01d9d6fdf27cf769e89b44e5b6f2be874
ncueto2 <ncueto2@illinois.edu>
2020-04-30 01:46:13 -0500

    bypass mrcnn model error limit

--- Mask_RCNN-master/mrcnn/model.py
+++ Mask_RCNN-master/mrcnn/model.py
@@ -1809,8 +1809,8 @@ def data_generator(dataset, config, shuffle=True, augment=False, augmentation=No
             logging.exception("Error processing image {}".format(
                 dataset.image_info[image_id]))
             error_count += 1
-            if error_count > 5:
-                raise
+            # if error_count > 5:
+            #     raise
 
 
 ############################################################
